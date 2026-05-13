b2cdd6bb9dd451452eabb24035c84614e8547885
Wadie Skaf <wadieskaf@gmail.com>
2026-05-13 11:51:29 +0200

    feat(03-03): wire all utils/__init__.py exports
    - Add imports for arff, features, general, and scaling utility modules
    - Export all 11 utility symbols (alphabetical __all__)
    - DataForm already exported from enums and root (wave 1)

--- src/tscollection/datasets/utils/__init__.py
+++ src/tscollection/datasets/utils/__init__.py
@@ -1,13 +1,33 @@
 """Utility functions for data processing."""
 
+from tscollection.datasets.utils.arff import (
+    process_df_according_to_dtypes,
+    read_arff_as_df,
+)
 from tscollection.datasets.utils.common import (
     compose,
+    flatten_list_of_np_arrays,
     FunctionComposer,
     get_num_samples_from_ts,
 )
+from tscollection.datasets.utils.features import extract_time_features
+from tscollection.datasets.utils.general import (
+    centralize_variable_length_series,
+    custom_collate_fn,
+    process_data_with_varying_sequence_lengths_single,
+)
+from tscollection.datasets.utils.scaling import create_data_scaler
 
 __all__ = [
     'FunctionComposer',
+    'centralize_variable_length_series',
     'compose',
+    'create_data_scaler',
+    'custom_collate_fn',
+    'extract_time_features',
+    'flatten_list_of_np_arrays',
     'get_num_samples_from_ts',
+    'process_data_with_varying_sequence_lengths_single',
+    'process_df_according_to_dtypes',
+    'read_arff_as_df',
 ]
