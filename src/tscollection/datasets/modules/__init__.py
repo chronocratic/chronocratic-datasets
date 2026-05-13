4dfe4a660daa36d5efcc35d5e8e48c7e801b952e
Wadie Skaf <wadieskaf@gmail.com>
2026-05-13 14:07:01 +0200

    feat(04-04): wire modules and classes __init__.py exports
    Wire all __all__ exports for the modules package and verify full
    import chain. 8 module class exports with ASCII-sorted __all__.

--- src/tscollection/datasets/modules/__init__.py
+++ src/tscollection/datasets/modules/__init__.py
@@ -1,3 +1,23 @@
 """LightningDataModule classes for time series datasets."""
 
-__all__ = []  # Populated in Phase 5
+from tscollection.datasets.modules.classes import (
+    BaseClassificationTimeSeriesDataModule,
+    BaseForecastingTimeSeriesDataModule,
+    BaseTimeSeriesDataModule,
+)
+from tscollection.datasets.modules.electricity import ElectricityLoadModule
+from tscollection.datasets.modules.ett import ETTDataModule
+from tscollection.datasets.modules.ucr import UCRClassificationDataModule
+from tscollection.datasets.modules.uea import UEAClassificationDataModule
+from tscollection.datasets.modules.weather import WeatherModule
+
+__all__ = [
+    'BaseClassificationTimeSeriesDataModule',
+    'BaseForecastingTimeSeriesDataModule',
+    'BaseTimeSeriesDataModule',
+    'ETTDataModule',
+    'ElectricityLoadModule',
+    'UCRClassificationDataModule',
+    'UEAClassificationDataModule',
+    'WeatherModule',
+]
--- src/tscollection/datasets/modules/classes/__init__.py
+++ src/tscollection/datasets/modules/classes/__init__.py
@@ -1,3 +1,15 @@
 """Abstract base classes for time series data modules."""
 
-__all__ = []  # Populated in Phase 5
+from tscollection.datasets.modules.classes.base import BaseTimeSeriesDataModule
+from tscollection.datasets.modules.classes.classification import (
+    BaseClassificationTimeSeriesDataModule,
+)
+from tscollection.datasets.modules.classes.forecasting import (
+    BaseForecastingTimeSeriesDataModule,
+)
+
+__all__ = [
+    'BaseClassificationTimeSeriesDataModule',
+    'BaseForecastingTimeSeriesDataModule',
+    'BaseTimeSeriesDataModule',
+]
